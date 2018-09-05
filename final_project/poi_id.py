1cc9a80ce7851960f846457973a6ed8d8e910da9
bruno <bruno.mansur@sysmap.com.br>
2018-09-04 23:33:03 -0300

    ajustes depois da primeira revisao

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -6,11 +6,15 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from tester import test_classifier
+
+import pandas as pd
 
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.cross_validation import train_test_split, cross_val_score, StratifiedShuffleSplit
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import recall_score, accuracy_score, precision_score
 from sklearn import cross_validation 
 import numpy as np
@@ -32,19 +36,19 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 print "Numero de dados no dataset: ", len(data_dict)
 
+df = pd.read_pickle("final_project_dataset.pkl")
+
 ### Task 2: Remove outliers
 del data_dict['TOTAL']
 
 ### Task 3: Create new feature(s)
 for data in data_dict.values():
     data['to_poi_msg_ratio'] = 0
-    data['from_poi_msg_ratio'] = 0
     if float(data['from_messages']) > 0:
         data['to_poi_msg_ratio'] = float(data['from_this_person_to_poi'])/float(data['from_messages'])
-    if float(data['to_messages']) > 0:
-        data['from_poi_msg_ratio'] = float(data['from_poi_to_this_person'])/float(data['to_messages'])
 
-features_list.extend(['to_poi_msg_ratio','from_poi_msg_ratio'])
+#Remover o comentario para adicionar as novas features
+features_list.extend(['to_poi_msg_ratio'])
 
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -59,10 +63,11 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 ## For local testing
-def resultado(clf):
+def resultado(algorithm, clf):
     # Provided to give you a starting point. Try a variety of classifiers.
     features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, random_state=42)
     clf.fit(features_train, labels_train)
+    print 'Algoritmo: ', algorithm
     print 'Melhor score: %0.3f' % clf.best_score_
     print 'Melhor parameters set:'
     best_parameters = clf.best_estimator_.get_params()
@@ -71,34 +76,59 @@ def resultado(clf):
         print '\t%s: %r' % (param_name, best_parameters[param_name])
         new_params[param_name] = best_parameters[param_name]
     predictions = clf.predict(features_test)
+    print 'Accuracy: ', accuracy_score(labels_test,predictions)
+    print 'Recall: ', recall_score(labels_test,predictions)
+    print 'Precision: ', precision_score(labels_test,predictions)
 
 # Provided to give you a starting point. Try a variety of classifiers.
-# features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
-
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
+'''
+kbest = SelectKBest(k = 6)
+kbest.fit(features, labels)
+f_list = zip(kbest.get_support(), features_list[1:], kbest.scores_)
+f_list = sorted(f_list, key=lambda x: x[2], reverse = True)
+
+import pprint
+print "K-best features:", 
+pprint.pprint(f_list)
+'''
+#updated feature list
+features_list = ['poi','exercised_stock_options','total_stock_value','bonus','salary','to_poi_msg_ratio','deferred_income']
+#features_list = ['poi','exercised_stock_options','total_stock_value','bonus','salary','deferred_income','long_term_incentive']
+
+'''
 kNeighbors = KNeighborsClassifier()
 parameters = {'n_neighbors': [2,3,4,5,6,7],
                       'algorithm': ['ball_tree', 'kd_tree', 'brute', 'auto'],
                       'weights': ['uniform', 'distance'],
                       'p': [2,3,4,5,6,7,8]}
 clf = GridSearchCV(kNeighbors, parameters, verbose=1, cv=10)
-resultado(clf)
+resultado("kNeighbors", clf)
 
-from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier()
 parameters = {'criterion': ["gini", "entropy"],
               'splitter': ['best', 'random'],
               'min_samples_split': [3,4,5]
              }
 clf = GridSearchCV(tree, parameters, verbose=1, cv=10)
-resultado(clf)
+resultado("DecisionTreeClassifier", clf)
 
-from sklearn.naive_bayes import GaussianNB
 bayes =  GaussianNB()
 parameters = {}
 clf = GridSearchCV(bayes, parameters, verbose=1, cv=10)
-resultado(clf)
+resultado("GaussianNB", clf)
 
+adaBoost = AdaBoostClassifier()
+parameters = {'algorithm': ["SAMME", "SAMME.R"],
+              'random_state': [1000,1200,1400,1600]
+            }
+clf = GridSearchCV(adaBoost, parameters, verbose=1, cv=10)
+resultado("AdaBoostClassifier", clf)
+'''
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
@@ -111,7 +141,11 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-clf = KNeighborsClassifier(algorithm="ball_tree", n_neighbors=3, p=3, weights="uniform")
+#clf = KNeighborsClassifier(algorithm="ball_tree", n_neighbors=3, p=3, weights="uniform")
+#clf = DecisionTreeClassifier(criterion='entropy', min_samples_split=3, splitter='random')
+#clf = AdaBoostClassifier(algorithm='SAMME', random_state=1000)
+clf = GaussianNB(priors=None)
+test_classifier(clf, my_dataset, features_list)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
